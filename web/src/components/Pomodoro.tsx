0c0d909836065641202d14aae6c8678b9d8e2dcd
PierreMasselot1 <pierrem@orohealth.me>
2022-05-30 00:39:51 -0400

    feat(web): basic shitty timer

--- web/src/components/Pomodoro.tsx
+++ web/src/components/Pomodoro.tsx
@@ -1,3 +1,43 @@
-export function Pomodoro(){
-    return <h1 className="underline text-green-300 ">HELLO WORLD</h1>
+import { useEffect, useState } from "react";
+
+export function Pomodoro() {
+  const [startTime, setStartTime] = useState<Date>(new Date());
+  const [minutesLeft, setMinutes] = useState<number>(25);
+  const [secondsLeft, setSeconds] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      timer();
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [startTime]);
+
+  function timer() {
+    const curTime = new Date();
+    let seconds = Math.floor(
+      (25 * 60000 - (curTime.getTime() - startTime.getTime())) / 1000
+    );
+    let minutes = Math.floor(seconds / 60);
+
+    setMinutes(minutes);
+    setSeconds(seconds - minutes * 60);
+  }
+
+  function resetTimer() {
+    console.log("reset timer");
+    setStartTime(new Date());
+  }
+  return (
+    <div>
+      <h1 className="underline text-green-300 text-7xl ">Pomodoro</h1>
+      <p className="mt-3 text-green-400 text-3xl">Time Left:</p>
+      <p className="text-9xl">
+        {" "}
+        {minutesLeft}:{secondsLeft}
+      </p>
+      <button onClick={resetTimer}>RESET TIMER</button>
+      <p></p>
+    </div>
+  );
 }
