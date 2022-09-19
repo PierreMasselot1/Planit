3ba0582f7c4b13a441b41cc9e4dff713ef2d875a
PierreMasselot1 <pierrem@orohealth.me>
2022-09-19 09:42:27 -0400

    feat(web): re add background, remove daisyui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,16 @@
-
-import { Pomodoro } from './components/pomodoro/Pomodoro';
+import { Pomodoro } from "./components/pomodoro/Pomodoro";
 
 function App() {
   return (
     <div className="flex flex-row">
-      
-      <div className=' w-25 h-screen'>
-        NAVIGATION BAR
-
-        PomodoroNavIcon
+      <div className=" w-25 h-screen bg-slate-700">
+        NAVIGATION BAR PomodoroNavIcon
       </div>
-      
-      <div className="w-full h-screen">
-        APPLICATION 
-        <Pomodoro/>
 
+      <div className="w-full h-screen bg-slate-800">
+        APPLICATION
+        <Pomodoro />
       </div>
-      
     </div>
   );
 }
