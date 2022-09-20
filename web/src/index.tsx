8412024c71539f6fe0e28cf6d6e2baba4079756c
PierreMasselot1 <pierrem@orohealth.me>
2022-09-20 11:35:50 -0400

    feat(web): add error page

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Root from "./routes/Root";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Pomodoro } from "./components/pomodoro/Pomodoro";
 import Home from "./routes/Home";
+import ErrorPage from "./routes/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage/>,
     children: [
       { path: "/", element: <Home /> },
       {
