a025bc3614b1651d17d9684b19c3c21b889d3155
PierreMasselot1 <pierrem@orohealth.me>
2022-09-25 23:35:38 -0400

    feat(backend): fixing shit for todo

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,13 +1,17 @@
 import express from "express";
 
-const express = require("express")
+const express = require("express");
 
-const router : express.Router = express.Router();
+const router: express.Router = express.Router();
 
-//sign-up
-router.post("/test", async (req, res) => {
-  //TODO input validation
-  res.json = {message:"TEST BACKEND ROUTE"}
+router.get("/test", async (req, res) => {
+  console.log("test");
+  res.json({ message: "TEST BACKEND ROUTE" });
+});
+
+router.post("/", async (req, res) => {
+  res.json({ message: "tried to post" });
 });
 
 module.exports = router;
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-import express from "express"
+import express from "express";
 
-const express = require("express")
+const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -8,13 +8,10 @@ const port = 5055;
 
 app.listen(port, console.log(`Listening on port ${port}`));
 
-import { Expression } from "typescript";
-//routes
-import * as todoRouter from "./routes/todo";
-
+const todoRouter = require("./routes/todo");
 //map routes
 app.use("/api/todo", todoRouter);
 
-app.get("/api/ping", (req : express.req, res :express.res) => {
+app.get("/api/ping", (req: express.req, res: express.res) => {
   res.json("beep boop");
 });
