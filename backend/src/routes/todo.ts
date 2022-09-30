7d13d429c60849c3bedd903bd630b46582284f37
PierreMasselot1 <pierrem@orohealth.me>
2022-09-29 21:26:18 -0400

    feat(backend): add todolist and todo item, add params to API call

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -11,19 +11,16 @@ router.get("/test", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  
-  
-  
-  
-  const text = `
-      INSERT INTO todo (id, message, description)
+  console.log(req.query);
+  const query = `
+      INSERT INTO todo (todo_list_id,title, description)
       VALUES ($1, $2, $3)
       RETURNING id
     `;
-  const values = [1, "message", "description"];
-  res = pool.query(text, values);
+  const values = [1, req.query.title, req.query.description];
+  await pool.query(query, values);
 
-  res.json({ message: "tried to post"});
+  res.json({ message: "tried to post" });
 });
 
 module.exports = router;
--- db/database.sql
+++ db/database.sql
@@ -1,5 +1,19 @@
+CREATE TABLE "todo_list"(
+    "id" SERIAL PRIMARY KEY,
+    "owner_id" integer NOT NULL
+);
 CREATE TABLE "todo" (
     "id" SERIAL PRIMARY KEY,
-    "message" VARCHAR(255) NOT NULL UNIQUE,
-    "description" VARCHAR(255) NOT NULL
-) WITH (OIDS = FALSE);
+    "todo_list_id" integer NOT NULL,
+    "title" VARCHAR(255),
+    "description" VARCHAR(255),
+    CONSTRAINT "todo_fk1" FOREIGN KEY ("todo_list_id") REFERENCES "todo_list"("id")
+);
+INSERT INTO todo_list ("id", "owner_id")
+VALUES (1, 1);
+INSERT INTO todo ("todo_list_id", "title", "description")
+VALUES (
+        1,
+        'THIS IS A DEVELOPMENT TEST TODO',
+        'THIS IS THE DESCRIPTION OF THE TEST TODO'
+    );
