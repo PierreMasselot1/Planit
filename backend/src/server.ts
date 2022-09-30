45d6b0dd4b7cbb08719b5670999c0dc8a4ffeb53
PierreMasselot1 <pierrem@orohealth.me>
2022-09-30 13:16:29 -0400

    feat(backend):more types

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
+import { Request, Response } from "express";
 const express = require("express");
 const app = express();
 
 app.use(express.json());
-const port = 5055;
+const port: number = 5055;
 
 app.listen(port, console.log(`Listening on port ${port}`));
 
@@ -10,6 +11,6 @@ const todoRouter = require("./routes/todo");
 //map routes
 app.use("/api/todo", todoRouter);
 
-app.get("/api/ping", (req, res) => {
+app.get("/api/ping", (req: Request, res: Response) => {
   res.json("beep boop");
 });
