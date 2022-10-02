da6ed3c7144d27f64e033bfc4544cf87e38f9962
PierreMasselot1 <pierrem@orohealth.me>
2022-10-01 20:28:51 -0400

    feat(web): cleaner taskbar

--- web/src/components/navbar/NavIcon.tsx
+++ web/src/components/navbar/NavIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavIcon(icon_title: string) {
   return (
-    <div className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 my-1">
+    <div className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mx-2 my-1">
       {icon_title}
     </div>
   );
--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavIcon from "./NavIcon";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-col mx-3">
+    <div className="flex flex-col m-3 py-1 bg-gray-700 h-96  rounded-lg">
       <Link to={"/"}>{NavIcon("Home")}</Link>
       <Link to={"pomodoro"}>{NavIcon("Pomodoro")}</Link>
       <Link to={"todo"} className="whitespace-nowrap">
