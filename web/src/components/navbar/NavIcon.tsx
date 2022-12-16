4353ef111cc109dc02344d9f9f834c1ad0577e99
PierreMasselot1 <pierrem@orohealth.me>
2022-12-15 22:55:21 -0500

    feat(web): disable links if not logged in

--- web/src/components/navbar/NavIcon.tsx
+++ web/src/components/navbar/NavIcon.tsx
@@ -1,8 +1,14 @@
 import React, { ReactNode } from "react";
 
-function NavIcon(element: string | ReactNode) {
+function NavIcon(element: string | ReactNode, disabled: Boolean = false) {
   return (
-    <div className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mx-2 my-1">
+    <div
+      className={`text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600  font-medium rounded-lg text-lg px-5 py-2.5 text-center mx-2 my-1 ${
+        disabled
+          ? "opacity-25"
+          : "hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800"
+      } `}
+    >
       {element}
     </div>
   );
--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -1,14 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import LoginButton from "../Auth/Login";
 import NavIcon from "./NavIcon";
 
 export default function Navbar() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="flex-auto flex-col m-3 py-1 bg-gray-700 rounded-lg">
-      <Link to={"/"}>{NavIcon("Home")}</Link>
-      <Link to={"pomodoro"}>{NavIcon("Pomodoro")}</Link>
+      <Link to={"/"}>{NavIcon("Home", !isAuthenticated)}</Link>
+      <Link to={"pomodoro"}>{NavIcon("Pomodoro", !isAuthenticated)}</Link>
       <Link to={"todo"} className=" whitespace-nowrap">
-        {NavIcon("Todo")}
+        {NavIcon("Todo", !isAuthenticated)}
       </Link>
       <div className="flex-auto m-auto"> {NavIcon(<LoginButton />)}</div>
     </div>
