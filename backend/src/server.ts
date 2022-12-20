925d72f231a6b53bcfe4d194677d673af342600e
PierreMasselot1 <pierrem@orohealth.me>
2022-12-20 00:55:54 -0500

    feat(backend): check for jwt token on todo route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,12 @@ import { Request, Response } from "express";
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 const checkJwt = auth({
-  audience: 'YOUR_API_IDENTIFIER',
+  audience: "planit-auth",
   issuerBaseURL: `https://dev-hfnpcabx1n3viyfj.us.auth0.com/`,
 });
 
@@ -25,7 +25,7 @@ app.listen(port, console.log(`Listening on port ${port}`));
 
 const todoRouter = require("./routes/todo");
 //map routes
-app.use("/api/todo", todoRouter);
+app.use("/api/todo", checkJwt, todoRouter);
 
 app.get("/api/ping", (req: Request, res: Response) => {
   res.json("beep boop");
