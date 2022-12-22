e3bb7a393ec6c4e35c47d1ea5a4c1dd80066e241
PierreMasselot1 <pierre.masselot.d@gmail.com>
2022-12-22 11:38:01 -0500

    feat(web): make entire button clickable

--- web/src/components/Auth/Login.tsx
+++ web/src/components/Auth/Login.tsx
@@ -5,16 +5,25 @@ const LoginButton = () => {
   const { isAuthenticated} = useAuth0();
   const { loginWithRedirect, logout } = useAuth0();
   return (
-    <div>
+    <div className="flex">
       {isAuthenticated ? (
-        <div>
-          
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log Out
-          </button>
-        </div>
+        <button
+          className={`w-full text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600  font-medium rounded-lg text-lg px-5 py-2.5 text-center mx-2 my-1
+       "hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800"
+     `}
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Log Out
+        </button>
       ) : (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button
+          className={`w-full text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600  font-medium rounded-lg text-lg px-5 py-2.5 text-center mx-2 my-1
+        "hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 "
+     } `}
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </button>
       )}
     </div>
   );
--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
           </div>
         )}
 
-        {NavIcon(<LoginButton />)}
+        <LoginButton />
       </div>
     </div>
   );
