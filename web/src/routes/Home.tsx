38e2dfec553aacfff62a85ca2aa8e4229acba649
PierreMasselot1 <pierre.masselot.d@gmail.com>
2023-01-05 11:42:18 -0500

    chore(web):remove stuff

--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className=" flex flex-auto bg-gray-700 rounded-lg mr-2 my-3 p-2 text-white flex-col justify-center text-center">
       <div className="text-9xl font-bold">Planit</div>
-      <div>This is the place to get more shit done!</div>
     </div>
   );
 }
