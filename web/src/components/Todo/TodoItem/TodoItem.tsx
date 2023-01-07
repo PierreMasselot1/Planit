c4fdd7f38b00b2b311a835fbc6c5a36fb9317916
PierreMasselot1 <pierrem@orohealth.me>
2023-01-06 19:21:59 -0500

    feat(web): remove log, cleaner close button

--- web/src/components/Todo/TodoItem/TodoItem.tsx
+++ web/src/components/Todo/TodoItem/TodoItem.tsx
@@ -1,19 +1,20 @@
 import { Todo } from "@shared/types/todo_types";
 
 export default function TodoItem(todo: Todo, deleteTodo: Function) {
-  function onToggle() {
-    console.log("toggled");
-  }
+  function onToggle() {}
   function onDestroy() {
     deleteTodo(todo.id);
   }
 
   return (
     <div className="bg-green-400 w-fit my-1 py-1 px-2 rounded ">
-      <input type="checkbox" onChange={onToggle} />
+      <input id="default-checkbox" type="checkbox" onChange={onToggle} />
       <label className="mx-2">{todo.title}</label>
       <label className="mx-2">{todo.description}</label>
-      <button onClick={onDestroy} className="bg-slate-50 rounded-full ">
+      <button
+        onClick={onDestroy}
+        className="text-xs bg-slate-700 hover:bg-slate-800 text-white font-bold py-1 px-2 rounded-full"
+      >
         X
       </button>
     </div>
