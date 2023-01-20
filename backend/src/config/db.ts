ac8c282f939472abac653f108b3a6411de091b81
PierreMasselot1 <pierrem@orohealth.me>
2023-01-19 23:41:20 -0500

    feat(backend): connect using connection string or host+port

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,24 @@
 import { Pool } from "pg";
-const pool = new Pool({
-  user: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: "planit",
-  password: "postgres",
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+
+const pool =
+  process.env.CONNECTION_STRING == ""
+    ? new Pool({
+        user: "postgres",
+        host: process.env.DB_HOST || "localhost",
+        database: "planit",
+        password: "postgres",
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      })
+    : new Pool({
+        connectionString: process.env.CONNECTION_STRING,
+      });
+
+pool.query("SELECT 1", (err, res) => {
+  if (err) {
+    console.error("Error connecting to the database: ", err.stack);
+  } else {
+    console.log("Successfully connected to the database");
+  }
 });
+
 module.exports = pool;
