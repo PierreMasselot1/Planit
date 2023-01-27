972e6527501c0282c4bb65338de7a0c602e88bf8
PierreMasselot1 <pierrem@orohealth.me>
2023-01-27 18:55:30 -0500

    feat(web): new habit input field

--- web/src/components/Habit/Habit.tsx
+++ web/src/components/Habit/Habit.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 function Habit() {
+  const [newHabit, setNewHabit] = useState("");
+
   return (
-    <div className="">
-        <div className="text-white text-2xl font-bold">Habit</div>
+    <div className="justify-start text-left">
+      <div className="text-white text-2xl font-bold">Habit</div>
+      <input
+        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        type="text"
+        placeholder="New habit"
+        value={newHabit}
+        onChange={(e) => setNewHabit(e.target.value)}
+      />
     </div>
   );
 }
