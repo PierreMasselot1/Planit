bbed70e650480ec7ca1c3d00b71d131376afb7a2
PierreMasselot1 <pierrem@orohealth.me>
2023-01-27 14:34:14 -0500

    feat(db): create habit table

--- db/database.sql
+++ db/database.sql
@@ -10,6 +10,20 @@ CREATE TABLE "todo" (
     "is_deleted" BOOLEAN,
     CONSTRAINT "todo_fk1" FOREIGN KEY ("todo_list_id") REFERENCES "todo_list"("id")
 );
+CREATE TABLE "habit_list"(
+    "id" SERIAL PRIMARY KEY,
+    "user_id" VARCHAR(255) NOT NULL,
+);
+CREATE TABLE "habit" (
+    "id" SERIAL PRIMARY KEY,
+    "habit_list_id" integer NOT NULL,
+    "title" VARCHAR(255),
+    "description" VARCHAR(255),
+    "is_deleted" BOOLEAN,
+    "streak" integer,
+    "last_completed" DATE,
+    CONSTRAINT "habit_fk1" FOREIGN KEY ("habit_list_id") REFERENCES "habit_list"("id")
+);
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255)
