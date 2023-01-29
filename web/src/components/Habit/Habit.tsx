37cb91bcdb6527ffab032db681783077ce6da5d7
PierreMasselot1 <pierrem@orohealth.me>
2023-01-29 15:44:09 -0500

    feat(web): local habit title and creation

--- web/src/components/Habit/Habit.tsx
+++ web/src/components/Habit/Habit.tsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 function Habit() {
-  const [newHabit, setNewHabit] = useState("");
+  const [habit_title, setHabitTitle] = useState("");
+  const [habits, setHabits] = useState("");
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    createHabit(habit_title);
+    setHabitTitle("");
+  };
+
+  async function createHabit(title: string) {
+    console.log("creating habit");
+  }
 
   return (
     <div className="justify-start text-left">
       <div className="text-white text-2xl font-bold">Habit</div>
-      <input
-        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-        type="text"
-        placeholder="New habit"
-        value={newHabit}
-        onChange={(e) => setNewHabit(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="text"
+          placeholder="New habit"
+          value={habit_title}
+          onChange={(e) => setHabitTitle(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
