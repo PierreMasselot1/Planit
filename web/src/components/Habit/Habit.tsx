a2e94c8feefda8b0ec2f22a1557eb9dde50c9d94
PierreMasselot1 <pierre.masselot.d@gmail.com>
2023-02-11 22:30:16 -0500

    feat(web): habit and scroll bar with proper flex

--- web/src/components/Habit/Habit.tsx
+++ web/src/components/Habit/Habit.tsx
@@ -1,6 +1,8 @@
 import Api from "../../helpers/api";
 import { SyntheticEvent, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Habit() {
   const api = new Api();
@@ -31,22 +33,28 @@ function Habit() {
   }
 
   return (
-    <div className="justify-start text-left">
+    <div className="flex flex-col justify-start text-left h-full">
       <div className="text-white text-2xl font-bold">Habit</div>
       <form onSubmit={handleSubmit}>
         <input
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 mb-2 block w-full appearance-none leading-normal"
           type="text"
           placeholder="New habit"
           value={habit_title}
           onChange={(e) => setHabitTitle(e.target.value)}
         />
       </form>
-      <div>
+      <div className="overflow-y-scroll overflow-x-hidden scrollbar flex flex-1 flex-col ">
         {habits &&
           habits?.map((habit: any, key: number) => (
-            <div className="bg-slate-300 w-fit my-1 py-1 px-2 rounded ">
-              <h2> {habit.title}</h2>
+            <div
+              className="bg-slate-300 my-1 mr-2 py-1 px-2 rounded flex  "
+              key={key}
+            >
+              <h2 className="break-words"> {habit.title}</h2>
+              <div className="ml-auto">
+                <FontAwesomeIcon icon={faCheck} />
+              </div>
             </div>
           ))}
       </div>
--- web/src/routes/Root.tsx
+++ web/src/routes/Root.tsx
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar/Navbar";
 
 export default function Root() {
   return (
-    <div className="h-screen overflow-y-hidden flex flex-col sm:flex-row bg-gray-800">
+    <div className="h-screen overflow-hidden flex flex-col sm:flex-row bg-gray-800">
       <Navbar />
-      <div className=" flex flex-auto bg-gray-700 rounded-lg mx-1 my-1 sm:mx-3 sm:my-3 p-2">
+      <div className="w-full bg-gray-700 rounded-lg mx-1 my-1 sm:mx-3 sm:my-3 p-2">
         <Outlet />
       </div>
     </div>
