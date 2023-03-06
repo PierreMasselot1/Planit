7534f3b819a3a03c807cb65a3229c97b770b3db1
PierreMasselot1 <pierrem@orohealth.me>
2023-03-06 00:29:23 -0500

    feat(web): pomodoro cleaner look

--- web/src/components/Todo/Todo.tsx
+++ web/src/components/Todo/Todo.tsx
@@ -61,35 +61,12 @@ export default function TodoListComponent() {
 
   return (
     <div className="flex-auto">
-      {todos &&
-        todos.map((todo: any, key: number) => (
-          <li className="list-none" key={key}>
-            {
-              <div className="bg-slate-300 w-1/2 my-1 py-1 px-2 rounded ">
-                <input
-                  id="default-checkbox"
-                  type="checkbox"
-                  onChange={onToggle}
-                />
-                <label className="mx-2">{todo.title}</label>
-                <label className="mx-2">{todo.description}</label>
-                <button
-                  onClick={() => deleteTodo(todo.id)}
-                  className="text-xs bg-slate-700 hover:bg-slate-800 text-white font-bold py-1 px-2 rounded-full"
-                >
-                  X
-                </button>
-              </div>
-            }
-          </li>
-        ))}
-
       <div className="mt-auto">
         <form onSubmit={handleSubmit}>
-          <div className="flex flex-row align-baseline  mb-4">
+          <div className="flex flex-row align-baseline  mb-4 flex-wrap">
             <input
               type="text"
-              className="h-full border rounded py-1 px-2 leading-tight focus:outline-none focus:border-teal-500"
+              className="mr-2 my-1 h-full border rounded py-1 px-2 leading-tight focus:outline-none focus:border-teal-500"
               placeholder="Title"
               id="title"
               value={title}
@@ -97,18 +74,43 @@ export default function TodoListComponent() {
             />
             <input
               type="text"
-              className="ml-2 h-full border rounded py-1 px-2 leading-tight focus:outline-none focus:border-teal-500"
+              className="mr-2 my-1 h-full border rounded py-1 px-2 leading-tight focus:outline-none focus:border-teal-500"
               id="description"
               placeholder="Description"
               value={description}
               onChange={(event) => setDescription(event.target.value)}
             />
-            <Button handleSubmit className="ml-2 px-2 py-0">
+            <Button handleSubmit className="my-1 px-2 py-0">
               Submit
             </Button>
           </div>
         </form>
       </div>
+      {todos &&
+        todos.map((todo: any, key: number) => (
+          <li className="list-none" key={key}>
+            {
+              <div className="bg-slate-300 sm:w-1/2 my-1 py-1 px-2 rounded flex ">
+                <input
+                  id="default-checkbox"
+                  className="mt-1 mb-auto"
+                  type="checkbox"
+                  onChange={onToggle}
+                />
+                <div>
+                  <label className="mx-2">{todo.title}</label>
+                  <label className="mx-2">{todo.description}</label>
+                </div>
+                <button
+                  onClick={() => deleteTodo(todo.id)}
+                  className="ml-auto my-auto text-xs h-6 bg-slate-700 hover:bg-slate-800 text-white font-bold py-1 px-2 rounded-full"
+                >
+                  X
+                </button>
+              </div>
+            }
+          </li>
+        ))}
     </div>
   );
 }
