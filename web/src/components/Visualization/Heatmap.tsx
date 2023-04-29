716b9d3330182f4aea02d531a9f29504d961dfd0
PierreMasselot1 <pierrem@orohealth.me>
2023-04-29 16:38:03 -0400

    feat(web): habit completion count heatmap

--- web/src/components/Visualization/Heatmap.tsx
+++ web/src/components/Visualization/Heatmap.tsx
@@ -1,35 +1,36 @@
-import { useState } from "react";
+import { faSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 const Heatmap = ({ dates }: { dates: Array<Date> | undefined }) => {
-  const currentDate = new Date();
   const [selectedYear, setSelectedYear] = useState(0);
-
-  const yearCounts: Array<Array<number>> = [];
+  const [curYearMaxCount, setcurYearMaxCount] = useState(0);
+  const [yearCounts, setYearCounts] = useState<Array<Array<number>>>([]);
   // Find the earliest date in the input array
-  if (dates) {
-    let earliestDate: Date = dates[0];
-    for (const date of dates) {
-      if (date < earliestDate) {
-        earliestDate = date;
-      }
-    }
-
-    const startYear = earliestDate.getFullYear();
-    const endYear = currentDate.getFullYear();
-
-    for (let year = 0; year <= endYear - startYear; year++) {
-      yearCounts.push(new Array(365).fill(0));
-      for (const date of dates) {
-        if (date.getFullYear() === startYear + year) {
-          yearCounts[year][getDayOfYear(date)] += 1;
+  useEffect(() => {
+    if (dates) {
+      const earliestDate: Date = dates[0];
+      const startYear = earliestDate.getFullYear();
+      const endYear = new Date().getFullYear();
+      let yearCountsTemp = new Array<Array<number>>();
+      for (let year = 0; year <= endYear - startYear; year++) {
+        yearCountsTemp.push(new Array(365).fill(0));
+        for (const date of dates) {
+          if (date.getFullYear() === startYear + year) {
+            yearCountsTemp[year][getDayOfYear(date)] += 1;
+          }
         }
       }
+
+      setYearCounts(yearCountsTemp);
     }
+  }, [dates]);
 
-    console.log(yearCounts); // Outputs an array with the counts for each day of the year
-  }
+  useEffect(() => {
+    if (yearCounts.length > 0)
+      setcurYearMaxCount(Math.max(...yearCounts[selectedYear]));
+  }, [yearCounts, selectedYear]);
 
-  // Helper function to get the day of year for a given date
   function getDayOfYear(date: Date) {
     const startOfYear = new Date(date.getFullYear(), 0, 0);
     const diff = date.getTime() - startOfYear.getTime();
@@ -37,17 +38,23 @@ const Heatmap = ({ dates }: { dates: Array<Date> | undefined }) => {
     const dayOfYear = Math.floor(diff / oneDay);
     return dayOfYear;
   }
-
   return (
-    <div className="flex flex-col">
-      test{" "}
-      <div className="flex flex-row break-words ">
-        {yearCounts &&
-        yearCounts[selectedYear].map((day, index) => (
-          <div key={index}>{day}</div>
+    <div className="flex flex-wrap">
+      {yearCounts?.length > 0 &&
+        yearCounts[selectedYear].map((completionCount, index) => (
+          <div key={index}>
+            <FontAwesomeIcon
+              className={` ${
+                (completionCount > 0 &&
+                  "text-teal-" +
+                    (Math.floor((completionCount / curYearMaxCount) * 5) + 1) *
+                      100) ||
+                "text-gray-500"
+              } align-top block m-px w-2 h-2`}
+              icon={faSquare}
+            />
+          </div>
         ))}
-      </div>
-      
     </div>
   );
 };
