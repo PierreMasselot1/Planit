188be9672504ac6bdbe3ef570e4cf2796d5dfc2b
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2023-05-15 23:28:23 -0400

    feat(mobile): more capacitor stuff

--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <div className=" text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-bold my-auto">
         Planit
       </div>
+      <div>
+        This is the home menu
+      </div>
     </div>
   );
 }
