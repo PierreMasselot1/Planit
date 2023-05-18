ab14ecc7cbaafb1c874789eb1a00a1a1bf55a9c2
PierreMasselot1 <pierrem@orohealth.me>
2023-05-17 22:02:47 -0400

    feat(web): mobile, no additional layer

--- web/src/routes/Root.tsx
+++ web/src/routes/Root.tsx
@@ -5,7 +5,7 @@ export default function Root() {
   return (
     <div className="h-screen overflow-hidden flex flex-col sm:flex-row bg-gray-800">
       <Navbar />
-      <div className="h-full sm:w-full sm:h-auto bg-gray-700 rounded-lg mx-2 my-2 p-2">
+      <div className="h-full sm:w-full sm:h-auto bg-gray-700 sm:rounded-lg sm:mx-2 sm:my-2 p-2">
         <Outlet />
       </div>
     </div>
