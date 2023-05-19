82fa55ad133296cb56f776d2be3af9ae1460f024
PierreMasselot1 <pierrem@orohealth.me>
2023-05-19 17:20:52 -0400

    feat(web): todo separate completed and incomplete Closes: https://github.com/PierreMasselot1/Planit/issues/9

--- web/src/routes/Todo/Todo.tsx
+++ web/src/routes/Todo/Todo.tsx
@@ -13,6 +13,8 @@ export default function TodoListComponent() {
   const api = new Api(useAuth0());
 
   const [todos, setTodos] = useState<Array<Todo>>([]);
+  const [completedTodos, setCompletedTodos] = useState<Array<Todo>>([]);
+  const [incompletedTodos, setIncompletedTodos] = useState<Array<Todo>>([]);
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [editId, setEditId] = useState<number | null>(null);
@@ -53,7 +55,10 @@ export default function TodoListComponent() {
 
   async function getAllTodos() {
     api.getTodos().then((data: AxiosResponse<TodoList, TodoList>) => {
-      setTodos((data as any).todos);
+      const todos: Array<Todo> = (data as any).todos;
+      setTodos(todos);
+      setCompletedTodos(todos.filter((todo) => todo.completed));
+      setIncompletedTodos(todos.filter((todo) => !todo.completed));
     });
   }
 
@@ -61,6 +66,94 @@ export default function TodoListComponent() {
     api.deleteTodo(id).then(() => {
       getAllTodos();
     });
+  }
+
+  function todoList(todos: Array<Todo>) {
+    return todos
+      ?.sort((a: Todo, b: Todo) => a.id - b.id)
+      .map((todo: Todo, key: number) => (
+        <li className="list-none" key={key}>
+          {
+            <div
+              className={`bg-slate-300 my-1 py-1 px-2 rounded flex mr-2 ${
+                todo.completed && "line-through"
+              }`}
+            >
+              <div className="flex flex-col w-full">
+                <div className="flex flex-row">
+                  {editId === todo.id ? (
+                    <textarea
+                      className="mx-2 border rounded leading-tight focus:outline-none focus:border-teal-500 resize-none w-full"
+                      placeholder="Description to edit"
+                      id="editTitle"
+                      value={editTitle}
+                      ref={editTitleAreaRef}
+                      rows={1}
+                      onChange={(event) => setEditTitle(event.target.value)}
+                    />
+                  ) : (
+                    <label className="mx-2 break-all break-words sm:text-lg">
+                      {todo.title}
+                    </label>
+                  )}
+
+                  <FontAwesomeIcon
+                    icon={faPen}
+                    onClick={() => {
+                      setEditId(todo.id);
+                      setEditTitle(todo.title);
+                      setEditDescription(todo.description);
+                    }}
+                    className="ml-auto m-2  hover:text-blue-500 cursor-pointer"
+                  />
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    onClick={() => {
+                      api
+                        .updateTodo(
+                          todo.id,
+                          undefined,
+                          undefined,
+                          !todo.completed
+                        )
+                        .then(() => {
+                          getAllTodos();
+                        });
+                    }}
+                    className={`m-2   ${
+                      todo.completed
+                        ? "hover:text-gray-500 text-green-500"
+                        : "hover:text-green-500"
+                    } cursor-pointer `}
+                  />
+
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    onClick={() => deleteTodo(todo.id)}
+                    className="m-2 cursor-pointer hover:text-red-500"
+                  />
+                </div>
+
+                {editId === todo.id ? (
+                  <textarea
+                    className="mx-2 border rounded leading-tight focus:outline-none focus:border-teal-500 resize-none w-full"
+                    placeholder="Description to edit"
+                    id="editDescription"
+                    value={editDescription}
+                    ref={editDescriptionAreaRef}
+                    rows={1}
+                    onChange={(event) => setEditDescription(event.target.value)}
+                  />
+                ) : (
+                  <label className="mx-2 break-all text-gray-800 ">
+                    {todo.description}
+                  </label>
+                )}
+              </div>
+            </div>
+          }
+        </li>
+      ));
   }
 
   return (
@@ -91,92 +184,8 @@ export default function TodoListComponent() {
         </form>
       </div>
       <div className="overflow-y-auto overflow-x-hidden scrollbar flex flex-1 flex-col w-1/2 min-w-fit">
-        {todos &&
-          todos
-            ?.sort((a: Todo, b: Todo) => a.id - b.id)
-            .map((todo: Todo, key: number) => (
-              <li className="list-none" key={key}>
-                {
-                  <div className="bg-slate-300 my-1 py-1 px-2 rounded flex mr-2">
-                    <div className="flex flex-col w-full">
-                      <div className="flex flex-row">
-                        {editId === todo.id ? (
-                          <textarea
-                            className="mx-2 border rounded leading-tight focus:outline-none focus:border-teal-500 resize-none w-full"
-                            placeholder="Description to edit"
-                            id="editTitle"
-                            value={editTitle}
-                            ref={editTitleAreaRef}
-                            rows={1}
-                            onChange={(event) =>
-                              setEditTitle(event.target.value)
-                            }
-                          />
-                        ) : (
-                          <label className="mx-2 break-all break-words sm:text-lg">
-                            {todo.title}
-                          </label>
-                        )}
-
-                        <FontAwesomeIcon
-                          icon={faPen}
-                          onClick={() => {
-                            setEditId(todo.id);
-                            setEditTitle(todo.title);
-                            setEditDescription(todo.description);
-                          }}
-                          className="ml-auto m-2  hover:text-blue-500 cursor-pointer"
-                        />
-                        <FontAwesomeIcon
-                          icon={faCheck}
-                          onClick={() => {
-                            api
-                              .updateTodo(
-                                todo.id,
-                                undefined,
-                                undefined,
-                                !todo.completed
-                              )
-                              .then(() => {
-                                getAllTodos();
-                              });
-                          }}
-                          className={`m-2   ${
-                            todo.completed
-                              ? "hover:text-gray-500 text-green-500"
-                              : "hover:text-green-500"
-                          } cursor-pointer `}
-                        />
-
-                        <FontAwesomeIcon
-                          icon={faXmark}
-                          onClick={() => deleteTodo(todo.id)}
-                          className="m-2 cursor-pointer hover:text-red-500"
-                        />
-                      </div>
-
-                      {editId === todo.id ? (
-                        <textarea
-                          className="mx-2 border rounded leading-tight focus:outline-none focus:border-teal-500 resize-none w-full"
-                          placeholder="Description to edit"
-                          id="editDescription"
-                          value={editDescription}
-                          ref={editDescriptionAreaRef}
-                          rows={1}
-                          onChange={(event) =>
-                            setEditDescription(event.target.value)
-                          }
-                        />
-                      ) : (
-                        <label className="mx-2 break-all text-gray-800 ">
-                          {todo.description}
-                        </label>
-                      )}
-                    </div>
-                  </div>
-                }
-              </li>
-            ))}
+        {todoList(incompletedTodos)}
+        <div className="mt-auto">{todoList(completedTodos)}</div>
       </div>
     </div>
   );
