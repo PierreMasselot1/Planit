d65e0189ce55527994fd9f0ccb9d6d1f50dfa5ac
PierreMasselot1 <pierrem@orohealth.me>
2023-05-24 16:56:36 -0400

    feat(web): scrollable navbar when overflow

--- web/src/routes/Navbar/Navbar.tsx
+++ web/src/routes/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="flex flex-row sm:flex-col justify-between my-1 sm:my-2 sm:ml-2 sm:py-1 sm:bg-gray-700 rounded-lg sm:w-60">
+    <div className="flex flex-row sm:flex-col justify-between my-1 sm:my-2 sm:ml-2 sm:py-1 sm:bg-gray-700 rounded-lg sm:w-60 overflow-auto">
       {NavIcon("", <FontAwesomeIcon icon={faHouse} />, "Home")}
       {NavIcon("pomodoro", <FontAwesomeIcon icon={faStopwatch} />, "Pomodoro")}
       {NavIcon(
