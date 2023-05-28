51996ebb125c4a3c9ebe07a3cb424b3fe416bcfd
PierreMasselot1 <pierre.masselot.d@gmail.com>
2023-05-27 17:13:47 -0700

    feat(backend): more skeleton routes for auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -16,5 +16,29 @@ router.post("/login", (req, res: Response<User | any>) => {
 
   res.json({ message: "tried to login" });
 });
+
+router.post("/register", (req, res: Response<User | any>) => {
+  console.log("register endpoint");
+  //TODO implement register with passport and password hashing
+  //Ideally hash and salt password on client then again on server
+  //Challenge based login? for now just being "safe" db side is
+  //Gonna have to be enough
+});
+
+router.post("/user", (req, res: Response<User | any>) => {
+  //RETURN USER
+});
+
+router.post("/logout",(req,res)=>{
+  //End session, return sucess
+})
+
+router.post("/deleteuser",(req,res)=>{
+  //Allow user to delete their account and all their data
+  //Maybe go extra and:
+  // 1 ASK if they want us to keep data for another time if they come back
+  // 2 if NO ask if they want a copy of all their data before deletion, data which they could then export on this or another account
+  // 3 confirm full deletion
+})
 module.exports = router;
 export default router;
