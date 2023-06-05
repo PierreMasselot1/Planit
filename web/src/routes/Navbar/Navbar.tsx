77c254534c1c321b4d32bffaceef14cc42d2b68c
PierreMasselot1 <pierrem@orohealth.me>
2023-06-06 00:22:55 +0200

    fix(web): fix navbar auth related issue

--- web/src/routes/Navbar/Navbar.tsx
+++ web/src/routes/Navbar/Navbar.tsx
@@ -10,37 +10,28 @@ import {
   faUser,
   faCalendarDay,
 } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { UserContext } from "../../components/Auth/userContext";
 export default function Navbar() {
-  const { isAuthenticated } = useAuth0();
-
+  const { user } = useContext(UserContext);
   return (
     <div className="flex flex-row sm:flex-col justify-between my-1 sm:my-2 sm:ml-2 sm:py-1 sm:bg-gray-700 rounded-lg sm:w-60 overflow-auto">
       {NavIcon("", <FontAwesomeIcon icon={faHouse} />, "Home")}
       {NavIcon("pomodoro", <FontAwesomeIcon icon={faStopwatch} />, "Pomodoro")}
-      {NavIcon(
-        "todo",
-        <FontAwesomeIcon icon={faCheck} />,
-        "Todo",
-        !isAuthenticated
-      )}
-      {NavIcon(
-        "habit",
-        <FontAwesomeIcon icon={faRepeat} />,
-        "Habit",
-        !isAuthenticated
-      )}
+      {NavIcon("todo", <FontAwesomeIcon icon={faCheck} />, "Todo", !user)}
+      {NavIcon("habit", <FontAwesomeIcon icon={faRepeat} />, "Habit", !user)}
       {NavIcon(
         "dailies",
         <FontAwesomeIcon icon={faCalendarDay} />,
         "Dailies",
-        !isAuthenticated
+        !user
       )}
       <div className="sm:mt-auto">
         {NavIcon(
           "profile",
           <FontAwesomeIcon icon={faUser} />,
           "Profile",
-          !isAuthenticated
+          !user
         )}
       </div>
       <div>
