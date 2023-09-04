511d9f3c955ac8dbcf57ec66eae340753e4aa66f
PierreMasselot1 <pierrem@orohealth.me>
2023-09-04 12:24:09 -0400

    heatmap add year picker

--- web/src/components/Visualization/Heatmap.tsx
+++ web/src/components/Visualization/Heatmap.tsx
@@ -1,43 +1,73 @@
 import { faSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import Button from "../Common/Button";
 
 const Heatmap = ({ dates }: { dates: Array<Date> | undefined }) => {
-  const [selectedYear] = useState(0);//TODO add selector
+  const [selectedYear, setSelectedYear] = useState<number>(
+    new Date().getFullYear()
+  );
   const [curYearMaxCount, setcurYearMaxCount] = useState(0);
-  const [yearCounts, setYearCounts] = useState<Array<Array<number>>>([]);
+  const [yearCounts, setYearCounts] = useState<{ [year: number]: number[] }>(
+    {}
+  );
 
-  const colorGradient = [
-    "text-primary-900",
-    "text-primary-800",
-    "text-primary-700",
-    "text-primary-600",
-    "text-primary-500",
-    "text-primary-400",
-  ];
+  const YearPicker = ({
+    startYear,
+    endYear,
+    selectedYear,
+    onChange,
+  }: {
+    startYear: number;
+    endYear: number;
+    selectedYear: number;
+    onChange: (year: number) => void;
+  }) => {
+    const years = Array.from(
+      { length: endYear - startYear + 1 },
+      (_, i) => endYear - i
+    );
+
+    return (
+      <div className="flex-col flex h-24 overflow-y-scroll overflow-x-hidden scrollbar pr-3">
+        {years.map((year) => (
+          <Button
+            key={year}
+            onClick={() => onChange(year)}
+            className={`w-fit my-0.5 ${
+              year === selectedYear ? "bg-primary-500" : ""
+            }`}
+          >
+            {year}
+          </Button>
+        ))}
+      </div>
+    );
+  };
   // Find the earliest date in the input array
   useEffect(() => {
     if (dates && dates.length > 0) {
       const earliestDate: Date = dates[0];
       const startYear = earliestDate.getFullYear();
       const endYear = new Date().getFullYear();
-      let yearCountsTemp = new Array<Array<number>>();
+      const yearCountsTemp: { [year: number]: number[] } = {};
       for (let year = 0; year <= endYear - startYear; year++) {
-        yearCountsTemp.push(new Array(365).fill(0));
-        for (const date of dates) {
-          if (date.getFullYear() === startYear + year) {
-            yearCountsTemp[year][getDayOfYear(date)] += 1;
-          }
+        yearCountsTemp[startYear + year] = Array(366).fill(0);
+      }
+      for (const date of dates) {
+        if (date.getFullYear() >= startYear && date.getFullYear() <= endYear) {
+          const year = date.getFullYear() - startYear;
+          yearCountsTemp[startYear + year][getDayOfYear(date)] += 1;
         }
       }
-
       setYearCounts(yearCountsTemp);
     }
   }, [dates]);
 
   useEffect(() => {
-    if (yearCounts.length > 0)
+    if (Object.keys(yearCounts).length > 0) {
       setcurYearMaxCount(Math.max(...yearCounts[selectedYear]));
+    }
   }, [yearCounts, selectedYear]);
 
   function getDayOfYear(date: Date) {
@@ -48,25 +78,39 @@ const Heatmap = ({ dates }: { dates: Array<Date> | undefined }) => {
     return dayOfYear;
   }
   return (
-    <div className="flex flex-wrap">
-      {yearCounts?.length > 0 &&
-        yearCounts[selectedYear].map((completionCount, index) => (
-          <div
-            key={index}
-            className={`${
-              completionCount > 0
-                ? colorGradient[
-                    Math.floor((completionCount / curYearMaxCount) * 5)
-                  ]
-                : "text-gray-500"
-            }`}
-          >
-            <FontAwesomeIcon
-              className={`align-top block m-px w-2 sm:w-3 h-2 sm:h-3 `}
-              icon={faSquare}
-            />
-          </div>
-        ))}
+    <div className="flex flex-row h-fit my-1">
+      <div className="w-fit h-full">
+        <div className="flex flex-wrap">
+          {Object.keys(yearCounts).length > 0 &&
+            yearCounts[selectedYear].map((completionCount, index) => (
+              <div
+                key={index}
+                className={`${
+                  completionCount > 0
+                    ? `text-primary-${
+                        Math.floor((completionCount / curYearMaxCount) * 5) *
+                        100
+                      }`
+                    : "text-gray-500"
+                }`}
+              >
+                <FontAwesomeIcon
+                  className={`align-top block m-px w-2 sm:w-3 h-2 sm:h-3 `}
+                  icon={faSquare}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+
+      <YearPicker
+        startYear={dates ? dates[0].getFullYear() : new Date().getFullYear()}
+        endYear={new Date().getFullYear()}
+        selectedYear={selectedYear}
+        onChange={(year) => {
+          setSelectedYear(year);
+        }}
+      />
     </div>
   );
 };
