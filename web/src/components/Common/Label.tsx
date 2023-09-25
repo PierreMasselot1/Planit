4523f3e1ae268e71e8f553b852ce222a87c82777
PierreMasselot1 <pierrem@orohealth.me>
2023-09-24 21:54:47 -0400

    better color picker

--- web/src/components/Common/Label.tsx
+++ web/src/components/Common/Label.tsx
@@ -1,7 +1,7 @@
-import { Label as Label_Tab } from "@shared/types/label_types";
+import { Label } from "@shared/types/label_types";
 
 export function LabelTab(
-  label: Label_Tab,
+  label: Label,
   onClick: () => void,
   isSelected: boolean
 ) {
@@ -9,13 +9,15 @@ export function LabelTab(
     <button
       onClick={onClick}
       key={label.id}
-      className={`px-2 py-1 mx-1 font-bold text-white bg-[${
-        label.color
-      }] rounded-md shadow-lg  focus:outline-none focus:shadow-outline ${
-        isSelected
-          ? "bg-primary-600 hover:bg-primary-700"
-          : "hover:bg-secondary-600"
-      } `}
+      style={{ backgroundColor: label.color, opacity: isSelected ? 1 : 0.5 }}
+      className={
+        `px-2 py-1 mx-1 font-bold text-white ${label.color}
+        rounded-md shadow-lg  focus:outline-none focus:shadow-outline ${
+          isSelected
+            ? "bg-primary-600 hover:bg-primary-700"
+            : "hover:bg-secondary-600"
+        }` as string
+      }
     >
       {label.title}
     </button>
